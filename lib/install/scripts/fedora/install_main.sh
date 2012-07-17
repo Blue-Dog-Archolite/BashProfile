b1e06b39fe8fb9ba05f59001e0fc4c8b2ca0d1d9
Robert R. Meyer <blue.dog.archolite@gmail.com>
2012-07-17 13:25:58 -0500

    Update master

--- lib/install/scripts/fedora/install_main.sh
+++ lib/install/scripts/fedora/install_main.sh
@@ -3,8 +3,6 @@ yum update
 yum install -y yum-plugin-fastestmirror
 rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
 rpm -ivh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
-rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-i386-1.0-1.noarch.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 yum groupinstall -y 'Development Tools'
 yum install -y gcc make git
 yum install -y rubygem-rails readline-devel openssl-devel ruby-mysql keepassx gkrellm amarok vlc pidgin meld gnome-do gnome-do-plugins vim gvim ruby ruby-devel ruby-libs ruby-mode ruby-rdoc ruby-irb ruby-ri ruby-docs ruby-mysql ruby-sqlite3 wget libxslt-devel libxml2-devel vim gvim parcellite libxml samba samba-client samba samba-client
