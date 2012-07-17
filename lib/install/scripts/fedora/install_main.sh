ab0912286bafd5ef994a4acd339cf0b2b18d115f
Robert R. Meyer <blue.dog.archolite@gmail.com>
2012-07-17 12:32:06 -0500

    Update master

--- lib/install/scripts/fedora/install_main.sh
+++ lib/install/scripts/fedora/install_main.sh
@@ -1,3 +1,4 @@
+su -
 yum update
 yum install -y yum-plugin-fastestmirror
 rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
@@ -5,12 +6,8 @@ rpm -ivh http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release
 rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-i386-1.0-1.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 yum groupinstall -y 'Development Tools'
-yum groupinstall -y 'Java Development'
-yum groupinstall -y "Office/Productivity"
 yum install -y gcc make git
-yum install -y nspluginwrapper.{i686,x86_64} alsa-plugins-pulseaudio.i686
-yum install -y rubygem-rails readline-devel openssl-devel ruby-mysql keepassx gkrellm amarok vlc pidgin meld gnome-do gnome-do-plugins vim gvim ruby ruby-devel ruby-libs ruby-mode ruby-rdoc ruby-irb ruby-ri ruby-docs ruby-mysql ruby-sqlite3 wget libxslt-devel libxml2-devel vim gvim homebank parcellite libxml samba samba-client samba samba-client
-yum install ccsm emerald-themes compizconfig-backend-gconf fusion-icon-gtk compiz-fusion-extras 
+yum install -y rubygem-rails readline-devel openssl-devel ruby-mysql keepassx gkrellm amarok vlc pidgin meld gnome-do gnome-do-plugins vim gvim ruby ruby-devel ruby-libs ruby-mode ruby-rdoc ruby-irb ruby-ri ruby-docs ruby-mysql ruby-sqlite3 wget libxslt-devel libxml2-devel vim gvim parcellite libxml samba samba-client samba samba-client
 yum install -y xine-lib-extras-freeworld
 
 exit
