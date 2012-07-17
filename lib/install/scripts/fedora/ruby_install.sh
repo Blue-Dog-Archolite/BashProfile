356a903001452aa9f21f43d56833246e0a6155c5
Robert R. Meyer <blue.dog.archolite@gmail.com>
2012-07-17 12:30:45 -0500

    Update master

--- lib/install/scripts/fedora/ruby_install.sh
+++ lib/install/scripts/fedora/ruby_install.sh
@@ -1,7 +1,4 @@
-wget -q http://rubyforge.org/frs/download.php/55066/rubygems-1.3.2.tgz
-tar xzf rubygems-1.3.2.tgz
-cd rubygems-1.3.2
-sudo ruby setup.rb
+sudo yum install rubygems
 sudo gem install rails
 sudo gem install mongrel mongrel_cluster
 sudo gem install json
