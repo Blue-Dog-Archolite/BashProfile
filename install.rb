d0e4c6e7c64facda7eaa8de314272e894f40c77b
Blue-Dog-Archolite <blue.dog.archolite@gmail.com>
2015-11-08 11:42:58 -0700

    INstaller update

--- install.rb
+++ install.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 
 dot_files_to_copy = %w{floorc pryrc bash_path floorc.json nvimrc vimrc guardrc bashrc bash_profile
-gitignore_global git-completion.bash synergy.conf vim bash_env bash_aliases ackrc irbrc}
+gitignore_global git-completion.bash synergy.conf bash_env bash_aliases ackrc irbrc}
 puts "Removing Old Files"
 dot_files_to_copy.each do |r|
-  puts `rm .#{r}`
+  puts `rm ~/.#{r}`
 end
+
+puts "Clear vim Directory"
+puts `rm -rf ~/.vim`
 
 puts "Creating Symlinks"
 dir_location = File.expand_path(File.dirname(__FILE__))
@@ -14,5 +17,11 @@ dot_locations = dir_location + '/dot_files/'
 
 
 dot_files_to_copy.each do |f|
-  puts `ln -s #{dot_locations + ".#{f}"} .#{f}`
+  file_name = ".#{f}"
+  path = dot_locations + file_name
+  command = "ln -s #{path} ~/#{file_name}"
+  puts `#{command}`
 end
+
+puts "setup Vim"
+puts `ln -s #{dot_locations + ".vim/"} ~/.vim`
