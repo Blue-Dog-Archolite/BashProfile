3260e1ab693d54a46cc5a8ce5f1738714e74a3e6
Blue-Dog-Archolite <blue.dog.archolite@gmail.com>
2018-02-12 10:55:23 -0700

    Updates for config files

--- home/.fzf.bash
+++ home/.fzf.bash
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/rmeyer/.vim/dein/repos/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="$PATH:/home/rmeyer/.vim/dein/repos/github.com/junegunn/fzf/bin"
+if [[ ! "$PATH" == */home/rmeyer/.vim/repos/github.com/junegunn/fzf/bin* ]]; then
+  export PATH="$PATH:/home/rmeyer/.vim/repos/github.com/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/rmeyer/.vim/dein/repos/github.com/junegunn/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/rmeyer/.vim/repos/github.com/junegunn/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/rmeyer/.vim/dein/repos/github.com/junegunn/fzf/shell/key-bindings.bash"
-
+source "/home/rmeyer/.vim/repos/github.com/junegunn/fzf/shell/key-bindings.bash"
